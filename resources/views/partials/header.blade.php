b8cb1cf7548f1226dd313a66dcf6892a233030a5
Furio Alessio <alefurio@hotmail.it>
2021-02-12 17:37:16 +0100

    scheletro header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,21 +1,39 @@
 <header>
-    <div class="logo">
-        <img src="{{ asset('images/logo.png') }}" alt="delivee-logo">
+    <div class="header-top">
+        <div class="logo">
+            <img src="{{ asset('images/logo.png') }}" alt="">
+        </div>
+
+        <nav>
+            <div class="">
+                @if (Route::has('login'))
+                    <div class="">
+                        @auth
+                            <a href="{{ url('/home') }}">Home</a>
+                        @else
+                            <a href="{{ route('login') }}">Login</a>
+
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}">Register</a>
+                            @endif
+                        @endauth
+                    </div>
+                @endif
+            </div>
+        </nav>
     </div>
 
-    <div class="flex-center position-ref full-height">
-        @if (Route::has('login'))
-            <div class="top-right links">
-                @auth
-                    <a href="{{ url('/home') }}">Home</a>
-                @else
-                    <a href="{{ route('login') }}">Login</a>
+    <div class="header-bottom">
+        <nav class="container">
+            <ul class="nav-menu">
+                <li><a href="#">Categoria</a></li>
+                <li><a href="#">Categoria</a></li>
+                <li><a href="#">Categoria</a></li>
+                <li><a href="#">Categoria</a></li>
+                <li><a href="#">Categoria</a></li>
+            </ul>
+        </nav>
+    </div>
 
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}">Register</a>
-                    @endif
-                @endauth
-            </div>
-        @endif
 
 </header>
