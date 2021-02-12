21ce3750194f5fa8b989d6830b696ffcb443fc41
Manuel <manuel.devito@icloud.com>
2021-02-12 18:29:40 +0100

    inizio footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,11 +8,16 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{ asset('css/app.css')}}">
     </head>
 
     <body>
-
         @extends('partials.header')
-
+        <main>
+            @yield('content')
+        </main>
+        @include('partials.footer')
     </body>
+
 </html>
