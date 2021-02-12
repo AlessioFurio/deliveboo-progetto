9b50decb3ed8bf52816505b7aba607a2033304b2
degortes <danilodegortes@gmal.com>
2021-02-12 16:39:26 +0100

    Create relazioni models

--- app/Course.php
+++ app/Course.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    //
+    public function dishes() {
+        return $this->hasMany('App\Dish');
+    }
 }
--- app/Payment.php
+++ app/Payment.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    public function restaurant() {
+        return $this->belongsTo('App\Restaurant');
+    }
+    public function paymentMethods() {
+        return $this->hasMany('App\Payment_method');
+    }
+    public function buyer() {
+        return $this->belongsTo('App\Buyer');
+    }
 }
